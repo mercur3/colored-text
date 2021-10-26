4b61c694dc3ea18907ae2bcd93aecdd44669d401
Andri Reveli <reveliandri@gmail.com>
2021-10-26 08:06:50 +0200

    #2 #3 implent Clone and Default

--- src/esthetics.rs
+++ src/esthetics.rs
@@ -1,4 +1,5 @@
 /// An `enum` that represents the possible style a text can be displayed in terminal
+#[derive(Clone)]
 pub enum Style {
 	Normal        = 0,
 	Bold          = 1,
@@ -12,7 +13,14 @@ pub enum Style {
 	Strikethrough = 9,
 }
 
+impl Default for Style {
+	fn default() -> Self {
+		return Style::Normal;
+	}
+}
+
 /// An `enum` that represents 8 possible colors that can be displayed in terminal
+#[derive(Clone)]
 pub enum Color {
 	Black  = 30,
 	Red    = 31,
@@ -22,4 +30,42 @@ pub enum Color {
 	Purple = 35,
 	Cyan   = 36,
 	White  = 37,
+}
+
+impl Default for Color {
+	fn default() -> Self {
+		return Color::White;
+	}
+}
+
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn clone_works() {
+		let a = Color::Black;
+		let b = a.clone();
+		matches!(a, _b);
+		matches!(b, _a);
+
+		let x = Style::Italic;
+		let y = x.clone();
+		matches!(x, _y);
+		matches!(y, _x);
+	}
+
+	#[test]
+	fn default_works() {
+		let a = Color::White;
+		let b = Color::default();
+		matches!(a, _b);
+		matches!(b, _a);
+
+		let x = Style::Normal;
+		let y = Style::default();
+		matches!(x, _y);
+		matches!(y, _x);
+	}
 }
